fdc08030e01467dbce43a86268160334b98d9c65
RahmaSohail <rahmasohail70@gmail.com>
2025-10-21 15:56:59 +0500

    updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5009;
  
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "*", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
